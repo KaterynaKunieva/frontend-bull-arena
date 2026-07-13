7b53daac0816c8ac42bd3d4b23efcd684ae63dcf
Kateryna <kunieva.kateryna@gmail.com>
2026-07-13 14:38:53 +0300

    review fixes

--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -1,83 +1,104 @@
 import { useEffect, useState, useRef, useCallback, memo } from "react";
 
-type BullRunEvent = CustomEvent<{position: number}>; 
+type BullRunEvent = CustomEvent<{ position: number }>;
+
+declare global {
+  interface DocumentEventMap {
+    bullRun: BullRunEvent;
+  }
+}
 
 type MatadorProps = {
   applause: number;
   matadorPosition: number;
   setMatarodPosition: (position: number | ((prev: number) => number)) => void;
-}
-
-function usePrevious<T>(value: T): T | undefined { 
-  const ref = useRef<T | undefined>(undefined);
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
-  return ref.current;
 };
 
-export const Matador = memo((props: Partial<MatadorProps>) => {  
+export const Matador = memo(
+  (props: Partial<MatadorProps>) => {
+    const audioRef = useRef<HTMLAudioElement>(null);
 
-  const [_, setApplause] = useState(false);
-  const prevApplause = usePrevious(props.applause); 
-  const audioRef = useRef<HTMLAudioElement>(null);
+    // handle moving
+    const bullRunHandler = useCallback(
+      (e: BullRunEvent) => {
+        if (
+          typeof props.matadorPosition === "undefined" ||
+          typeof props.setMatarodPosition === "undefined"
+        ) {
+          return;
+        }
 
-  // handle moving 
-  const bullRunHandler = useCallback((event: Event) => {
-    if (typeof props.matadorPosition === "undefined" || typeof props.setMatarodPosition === "undefined") {
-      return;
-    }
+        const bullPosition = e.detail.position;
 
-    const e = event as BullRunEvent;
-    const bullPosition = e.detail.position;
+        if (bullPosition === props.matadorPosition) {
+          let newPosition = props.matadorPosition;
+          while (bullPosition === newPosition) {
+            newPosition = Math.floor(Math.random() * 9);
+          }
+          props.setMatarodPosition(() => {
+            console.log(
+              `Matador is moving from ${props.matadorPosition} to ${newPosition}`,
+            );
+            return newPosition;
+          });
+        }
+      },
+      [props.matadorPosition, props.setMatarodPosition],
+    );
 
-    if (bullPosition === props.matadorPosition) {
-      let newPosition = props.matadorPosition;
-      while (bullPosition === newPosition) {
-        newPosition = Math.floor(Math.random() * 9);
-      }
-      props.setMatarodPosition(() => {
-        console.log(`Matador is moving from ${props.matadorPosition} to ${newPosition}`);
-        return newPosition;
-      });
-    }
-  }, [props.matadorPosition, props.setMatarodPosition]);
-  
-  useEffect(() => {
-    document.addEventListener("bullRun", bullRunHandler);
-    
-    return () => {
-      document.removeEventListener("bullRun", bullRunHandler);
-    }
-    
-  }, [bullRunHandler]); 
-  
-  // handle applause 
-  useEffect(() => {
-    const audio = audioRef.current; 
-    
-    return () => {
-      if (audio) {
-        audio.pause();
-        audio.currentTime = 0;
-      }
-    };
-  }, []);
-    
-  useEffect(() => {
-    if (props.applause === 3 && prevApplause !== 3) {
-      setApplause((prev) => !prev);
+    useEffect(() => {
+      document.addEventListener("bullRun", bullRunHandler);
+
+      return () => {
+        document.removeEventListener("bullRun", bullRunHandler);
+      };
+    }, [bullRunHandler]);
+
+    // handle applause
+    useEffect(() => {
       const audio = audioRef.current;
+
       if (audio) {
         audio.play().catch(() => {
           console.log(`Error playing applause`);
         });
       }
-    }
-  }, [props.applause]);
 
-  return <>
-    <img src="matador.svg" /> 
-    {/* <audio ref={audioRef} style={{ display: "none" }} src={`applause-3.wav`} autoPlay controls></audio> */}
-  </>;
-});
+      return () => {
+        if (audio) {
+          audio.pause();
+          audio.currentTime = 0;
+        }
+      };
+    }, [props.applause]);
+
+    return (
+      <>
+        <img src="matador.svg" />
+        <audio
+          ref={audioRef}
+          style={{ display: "none" }}
+          src={`applause-${props.applause}.wav`}
+          autoPlay
+          controls
+        ></audio>
+      </>
+    );
+  },
+  (prevProps, nextProps) => {
+    // don't render conditions
+    if (
+      (prevProps.applause !== nextProps.applause && nextProps.applause !== 3) || // next is not 3
+      (prevProps.applause === 3 && nextProps.applause === 3) // prev and next is 3
+    ) {
+      return true;
+    }
+
+    // simple props comparison
+    return (
+      prevProps.applause === nextProps.applause &&
+      prevProps.matadorPosition === nextProps.matadorPosition &&
+      prevProps.setMatarodPosition === nextProps.setMatarodPosition
+    );
+  },
+);
--- src/components/OldMatador.tsx
+++ src/components/OldMatador.tsx
@@ -1,38 +1,45 @@
-import { PureComponent, createRef } from "react";
+import { Component, createRef } from "react";
 
 type MatadorProps = {
   applause: number;
   matadorPosition: number;
   setMatarodPosition: (position: number | ((prev: number) => number)) => void;
-}
+};
 
 type MatadorState = {
   applause: boolean;
-} 
+};
 
-type BullRunEvent = CustomEvent<{position: number}>; 
+type BullRunEvent = CustomEvent<{ position: number }>;
 
-export class OldMatador extends PureComponent<Partial<MatadorProps>, MatadorState> {
-  private audioRef: React.RefObject<HTMLAudioElement> = createRef<HTMLAudioElement>();
+declare global {
+  interface DocumentEventMap {
+    bullRun: BullRunEvent;
+  }
+}
+
+export class OldMatador extends Component<Partial<MatadorProps>, MatadorState> {
+  private audioRef: React.RefObject<HTMLAudioElement> =
+    createRef<HTMLAudioElement>();
 
   constructor(props: Partial<MatadorProps>) {
     super(props);
     this.audioRef = createRef<HTMLAudioElement>();
     this.state = {
       applause: false,
-    }
-  }
-  
-  componentDidMount() { 
-    // handle moving
-    document.addEventListener("bullRun", this.bullRunHandler); 
+    };
   }
 
-  componentWillUnmount() { 
+  componentDidMount() {
+    // handle moving
+    document.addEventListener("bullRun", this.bullRunHandler);
+  }
+
+  componentWillUnmount() {
     // handle moving
     document.removeEventListener("bullRun", this.bullRunHandler);
 
-    // handle applause 
+    // handle applause
     const audio = this.audioRef.current;
     if (audio) {
       audio.pause();
@@ -40,23 +47,36 @@ export class OldMatador extends PureComponent<Partial<MatadorProps>, MatadorStat
     }
   }
 
-  componentDidUpdate(prevProps: Partial<MatadorProps>) {
-    // handle applause 
-    if (prevProps.applause !== this.props.applause) {
-      if (this.props.applause === 3 && prevProps.applause !== 3) {
-        this.setState({ applause: !this.state.applause });
-        const audio = this.audioRef.current;
-        if (audio) {
-          audio.play().catch(() => {
-            console.log(`Error playing applause`);
-          });
-        }
-      } 
-    }  
+  shouldComponentUpdate(nextProps: Partial<MatadorProps>) {
+    // don't render conditions
+    if (
+      (this.props.applause !== nextProps.applause &&
+        nextProps.applause !== 3) || // next is not 3
+      (this.props.applause === 3 && nextProps.applause === 3) // prev and next is 3
+    ) {
+      return false;
+    } else {
+      const audio = this.audioRef.current;
+      if (audio) {
+        audio.play().catch(() => {
+          console.log(`Error playing applause`);
+        });
+      }
+    }
+
+    // simple props comparison
+    return (
+      nextProps.applause !== nextProps.applause ||
+      nextProps.matadorPosition !== nextProps.matadorPosition ||
+      nextProps.setMatarodPosition !== nextProps.setMatarodPosition
+    );
   }
 
-  private bullRunHandler = (event: Event) => { 
-    if (typeof this.props.matadorPosition === "undefined" || typeof this.props.setMatarodPosition === "undefined") {
+  private bullRunHandler = (event: Event) => {
+    if (
+      typeof this.props.matadorPosition === "undefined" ||
+      typeof this.props.setMatarodPosition === "undefined"
+    ) {
       return;
     }
 
@@ -69,16 +89,26 @@ export class OldMatador extends PureComponent<Partial<MatadorProps>, MatadorStat
         newPosition = Math.floor(Math.random() * 9);
       }
       this.props.setMatarodPosition(() => {
-        console.log(`Matador is moving from ${this.props.matadorPosition} to ${newPosition}`);
+        console.log(
+          `Matador is moving from ${this.props.matadorPosition} to ${newPosition}`,
+        );
         return newPosition;
       });
     }
-  };  
+  };
 
   render() {
-    return <>
-        <img src="matador.svg" /> 
-        <audio ref={this.audioRef} style={{ display: "none" }} src={`applause-3.wav`} autoPlay controls></audio>
-    </>;
+    return (
+      <>
+        <img src="matador.svg" />
+        <audio
+          ref={this.audioRef}
+          style={{ display: "none" }}
+          src={`applause-3.wav`}
+          autoPlay
+          controls
+        ></audio>
+      </>
+    );
   }
 }
